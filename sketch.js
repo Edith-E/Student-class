b25e76a5daee6b7d0a92f04cc7b7b27b1fb116ed
Edith-E <69354983+Edith-E@users.noreply.github.com>
2023-04-05 17:36:16 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var student1
+function setup() {
+  createCanvas(400,400);
+student1 = new Student("Carl",14,"3rd grade")
+student1.display()
+}
+
+function draw() 
+{
+  background(30);
+  
+}
---
+++ student.js
@@ -0,0 +1,18 @@
+
+class Student
+{
+   constructor(name , age , grade){
+   this.name = name
+   this.age = age
+   this.grade = grade 
+   }
+   display(){
+    console.log("student details")
+    console.log("name:"+ this.name)
+    console.log("age:" + this.age)
+    console.log("grade:" + this.grade)
+   }
+}
+
+
+
